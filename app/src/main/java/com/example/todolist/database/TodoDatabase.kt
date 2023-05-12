d89965abbbb96f1cd5d7d6b6d41753bb447fb127
mikrotyan <foggycurse1@gmail.com>
2023-05-13 00:03:54 +0700

    add local database for tasks (auth dosnt work)

---
+++ app/src/main/java/com/example/todolist/database/TodoDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.todolist.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.todolist.models.Todo
+import com.example.todolist.models.TodoDao
+
+
+@Database(entities = arrayOf(Todo::class), version = 1)
+    abstract class TodoDatabase : RoomDatabase() {
+        abstract fun getTodoDao(): TodoDao
+
+    companion object {
+        @Volatile
+            private var INSTANCE: TodoDatabase? = null
+
+        fun getDatabase(context: Context): TodoDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TodoDatabase::class.java,
+                    "DATABASE_NAME"
+                ).build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+}
